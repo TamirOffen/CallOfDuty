4a5c9598053b4a67af1752b2fa5f801e8cbe5458
Tamir <hofftom342@gmail.com>
2025-02-11 12:59:40 +0200

    Add connection to db and checkpoint

--- routes.js
+++ routes.js
@@ -5,6 +5,16 @@ async function routes (fastify, options) {
         reply.code(200);
         return {status: "ok"}
     });
+
+    // health db checkpoint
+    fastify.get('/health/db', async (request, reply) => {
+        // check if connected to db
+        await fastify.mongo.db.admin().ping();
+        // connection to db is succesful
+        reply.code(200);
+        return {status: "ok"};
+
+    });
 };
 
 export default routes;
--- server.js
+++ server.js
@@ -2,6 +2,14 @@ import { createFastifyApp } from './app.js';
 
 const fastify = createFastifyApp();
 const port = process.env.PORT ?? 3000;
+
+// SIGINT is ctrl + c
+process.on('SIGINT', async () => {
+    console.log('\nStarting to shut down server...');
+    await fastify.close();
+    process.exit(0);
+});
+
 fastify.listen({ port }, (err, address) => {
     if (err) {
         console.error(err);
