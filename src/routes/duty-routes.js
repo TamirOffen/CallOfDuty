a85c891e62fbc9d416575a5d28fe31adcf5564c6
Tamir <hofftom342@gmail.com>
2025-04-09 09:18:40 +0300

    Create GET /duties endpoint with search filters

--- src/routes/duty-routes.js
+++ src/routes/duty-routes.js
@@ -1,5 +1,8 @@
 import { createDuty } from "../models/duty.js";
-import { postDutySchema } from "../schemas/duty-schemas.js";
+import {
+	getDutyByQuerySchema,
+	postDutySchema,
+} from "../schemas/duty-schemas.js";
 
 export async function dutyRoutes(fastify) {
 	fastify.post("/", { schema: postDutySchema }, async (request, reply) => {
@@ -13,4 +16,20 @@ export async function dutyRoutes(fastify) {
 			return reply.status(400).send({ message: err.message });
 		}
 	});
+
+	fastify.get("/", { schema: getDutyByQuerySchema }, async (request, reply) => {
+		const { constraints, ...otherProps } = request.query;
+		const filter = otherProps;
+		if (constraints?.length) filter.constraints = { $all: constraints.split(",") };
+		request.log.info({ filter }, "Searching for duties by query");
+
+		const duties = Object.keys(filter).length > 0
+				? await fastify.mongo.db.collection("duties").find(filter).toArray()
+				: [];
+		if (duties.length) request.log.info("No duties found");
+		else request.log.info({ duties }, "Duties found");
+
+		return reply.status(200).send(duties);
+	});
+
 }
--- src/schemas/duty-schemas.js
+++ src/schemas/duty-schemas.js
@@ -54,4 +54,24 @@ const postDutySchema = {
 	},
 };
 
-export { postDutySchema };
+const getDutyByQuerySchema = {
+	querystring: dutySchema.omit({
+			_id: true,
+			constraint: true,
+			soldiers: true,
+			statusHistory: true,
+			createdAt: true,
+			updatedAt: true,
+		})
+		.extend({
+			constraints: z.string(),
+		}).partial(),
+	response: {
+		200: z.array(dutySchema),
+	},
+};
+
+export {
+	postDutySchema,
+	getDutyByQuerySchema,
+};
